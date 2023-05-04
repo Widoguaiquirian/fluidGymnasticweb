6358274d85c77c5e62706441f726641e2485f5db
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2023-05-04 08:55:17 -0400

    Adding dark and light mode

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,18 @@
 "use strict";
+const header = document.querySelector(".header");
+const hamburguerMenu = document.querySelector(".header__menu");
+
+hamburguerMenu.addEventListener("click", function () {
+   header.classList.toggle("open");
+});
+
+const checkbox = document.querySelector('input[name="theme"]');
+const htmlElement = document.documentElement;
+
+checkbox.addEventListener("change", function () {
+   if (checkbox.checked) {
+      htmlElement.setAttribute("data-theme", "dark");
+   } else {
+      htmlElement.setAttribute("data-theme", "light");
+   }
+});
