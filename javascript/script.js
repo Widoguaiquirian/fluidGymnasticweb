7bc2ac1e2e94607616af8b8a9d85facec5808fc7
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2023-05-12 23:45:46 -0400

    Testimonial section

--- javascript/script.js
+++ javascript/script.js
@@ -19,3 +19,28 @@ checkbox.addEventListener("change", function () {
       htmlElement.setAttribute("data-theme", "light");
    }
 });
+
+// Testimonials functionality
+const slides = document.querySelector(".testimonials__content--slider").children;
+const indicatorImgs = document.querySelector(".testimonials__content--indicator").children;
+
+for (let i = 0; i < indicatorImgs.length; i++) {
+   indicatorImgs[i].addEventListener("click", function () {
+      console.log(indicatorImgs);
+      // console.log(this.getAttribute("data-id"));
+      // Gettnig the slide images
+      for (let j = 0; j < indicatorImgs.length; j++) {
+         indicatorImgs[j].classList.remove("active");
+      }
+
+      this.classList.add("active");
+
+      // Getting the next slide
+      const id = this.getAttribute("data-id");
+      for (let k = 0; k < slides.length; k++) {
+         slides[k].classList.remove("active");
+      }
+
+      slides[id].classList.add("active");
+   });
+}
